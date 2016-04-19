1a315ac7593d5e259975ead768ffb714f4aa78ad
PI de Eli <eli@tv>
2016-04-19 01:13:09 +0000

    Camara

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+var spawn = require("child_process").spawn;
 var socket = require('socket.io-client')('http://10.0.1.21:7000', {
 	reconnection: true,
 	reconnectionDelay: 200,
 	reconnectionDelayMax: 200
 });
+
+var _in, start = function(){
+	_in = setInterval(function(){
+		//console.log('image');
+		//socket.emit('image', {data:true});
+	}, 1000)
+}, stop = function(){
+	clearInterval(_in);
+}
+
 socket.on('connect', function(){
 	console.log('conectado');
+	start();
 });
 
 
@@ -34,4 +46,6 @@ socket.on('tv:ping', function(data){
 	console.log('ping...pong');
 });
 
-socket.on('disconnect', function(){});
+socket.on('disconnect', function(){
+	stop();
+});
